e6127df1fd94e181d69af5b7452c216c8f68ed66
easy28g <fj@tk$q]3v>
2023-07-17 03:28:47 +0600

    uploading images

--- src/main/java/com/example/demosecurity/domains/Message.java
+++ src/main/java/com/example/demosecurity/domains/Message.java
@@ -22,6 +22,8 @@ public class Message {
     @JoinColumn(name = "user_id")
     private User author;
 
+    private String filename;
+
     public Message(){
 
     }
@@ -62,6 +64,14 @@ public class Message {
 
     public void setAuthor(User author) {
         this.author = author;
+    }
+
+    public String getFilename() {
+        return this.filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
     }
 
 }
--- src/main/java/com/example/demosecurity/securingweb/MvcConfig.java
+++ src/main/java/com/example/demosecurity/securingweb/MvcConfig.java
@@ -1,17 +1,29 @@
 package com.example.demosecurity.securingweb;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer {
+
+	@Value("${upload.path}")
+    private String uploadPath;
 
 	public void addViewControllers(ViewControllerRegistry registry) {
 		// registry.addViewController("/home").setViewName("home");
 		// registry.addViewController("/").setViewName("home");
 		// registry.addViewController("/hello").setViewName("hello");
 		registry.addViewController("/login").setViewName("login");
+	}
+
+	public void addResourceHandlers(ResourceHandlerRegistry registry){
+		registry.addResourceHandler("/uploads/**")
+				.addResourceLocations("file:" + uploadPath + "/");
 	}
 
 }
